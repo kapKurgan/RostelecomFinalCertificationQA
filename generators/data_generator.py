779ab9bccc5b7a610faa5107e1890ca0272cba45
kapKurgan <kkaapp@yandex.ru>
2025-12-05 13:22:40 +0500

    Добавить метод generate_single_user со случайными данными

--- generators/data_generator.py
+++ generators/data_generator.py
@@ -3,6 +3,21 @@ import random
 from typing import Dict, Any, List
 
 class UserDataGenerator:
+    """Генератор тестовых данных для пользователей"""
+
     def __init__(self, locale: str = "en_US"):
         self.fake = Faker(locale)
         self.user_statuses = [0, 1, 2, 3]
+
+    def generate_single_user(self, username: str = None) -> Dict[str, Any]:
+        """Генерация данных одного пользователя"""
+        return {
+            "id": random.randint(1000, 99999),
+            "username": username or self.fake.user_name(),
+            "firstName": self.fake.first_name(),
+            "lastName": self.fake.last_name(),
+            "email": self.fake.email(),
+            "password": self.fake.password(),
+            "phone": self.fake.phone_number(),
+            "userStatus": random.choice(self.user_statuses)
+        }
